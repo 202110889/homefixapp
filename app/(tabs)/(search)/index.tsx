0295c3e4bf1e790b962c9d17aa895944739a39fb
Nuri <hanuri1435@gmail.com>
2025-05-26 14:49:02 +0900

    [UPDATE] add Image to Camera index

--- app/(tabs)/(search)/index.tsx
+++ app/(tabs)/(search)/index.tsx
@@ -1,11 +1,20 @@
+import { useLocalSearchParams } from "expo-router";
 import React, { createContext, useState } from "react";
+import { Image } from "react-native";
 
 export const MediaContext = createContext("");
 const Search = () => {
-  const [media, setMedia] = useState([]);
+  const [media, setMedia] = useState(useLocalSearchParams().media);
   // useLocalSearchParams().media;
   console.log(MediaContext);
-  return <MediaContext.Provider value={media}></MediaContext.Provider>;
+  return (
+    <>
+      <Image
+        source={{ uri: `file://${media}` }}
+        style={{ width: "100%", height: "80%", resizeMode: "contain" }}
+      />
+    </>
+  );
 };
 
 export default Search;
